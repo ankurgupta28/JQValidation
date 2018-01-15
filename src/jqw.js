8fe0fae2af8cb375ea362a41b8122d9c7f9d009e
nitesh_pspl@outlook.com <nitesh_pspl@outlook.com>
2018-01-15 16:20:18 +0530

    Basic layout for JQW

---
+++ src/jqw.js
@@ -0,0 +1,39 @@
+// jQueryValidations Library 
+// Alias Name : jqw
+
+(function(window) {
+    // We can enable the strict mode commenting the following line  
+    // 'use strict';
+
+
+    // This function will contain all our code
+    function jqw() {
+        // This variable will be accessible to the end user.
+        var _jqwObject = {
+            validate: _validate,
+            version: '1.0',
+        };
+
+        // This variable will be inaccessible to the end user, this can be used in the scope of library.
+        var defaults = {
+            dataType: {
+                number: 'number',
+                date: 'date',
+                email: 'email'
+            },
+        };
+
+        // function to validate complete form
+        function _validate(formName) {
+            //logic to validate form
+            return true;
+        };
+
+        return _jqwObject;
+    }
+
+    // We need that our library is globally accesible, so we save in the window with name 'jqw'
+    if (typeof(window.jqw) === 'undefined') {
+        window.jqw = jqw();
+    }
+})(window); // We send the window variable withing our function
